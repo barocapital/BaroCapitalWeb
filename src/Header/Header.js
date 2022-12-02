36e1c2e48251ae8e43f1315c944e84a9f1a2abb5
Dylan Molina <dylanivanmolina@gmail.com>
2022-12-01 23:04:18 -0600

    Installed Tailwind CSS and updated from default create-reat-app UI

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+      <>
+        <div className="bg-black text-white dark:text-white flex justify-center items-center h-40">
+           <div>
+               <h1 className="mt-3 mb-8 sm:mr-2 md:text-6xl lg:mt-8 font-bold text-7xl pt-4 pb-2 bg-clip-text text-transparent bg-gradient-to-tr from-orange-500 to-yellow-500">Baro Capital</h1>
+               <p>(Landing page in progress.)</p>
+           </div>
+        </div>
+
+      </>
+  )
+}
+
+export default Header;
